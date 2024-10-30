e76c51ab7c6e2c23fb38e83b907bbb7b5674f48e
Szymon Sulejczak <contact@szymonsulejczak.com>
2024-10-30 21:17:19 +0100

    add player class

---
+++ game/player.py
@@ -0,0 +1,18 @@
+class Player:
+    def __init__(self, name, balance):
+        self.name = name
+        self._balance = balance # chronione saldo
+
+    @property
+    def balance(self):
+        return self._balance
+
+    # dodwanie środków
+    def add_wins(self, amount):
+        self._balance += amount
+
+    # odejmowanie określonej stawki
+    def substract_rate(self, rate):
+        if rate > self._balance:
+            raise ValueError("Niewystarczające środki na koncie.")
+        self._balance -= rate
