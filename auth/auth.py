945e3edcebb28d2b96e0c96974563a1532baa0b6
Szymon Sulejczak <contact@szymonsulejczak.com>
2024-10-30 20:22:00 +0100

    add game ranking configuration and add optymalization

--- auth/auth.py
+++ auth/auth.py
@@ -1,7 +1,5 @@
 import hashlib
-import sqlite3
 import colors.colors as color
-
 
 class User:
     def __init__(self, username, password):
@@ -13,21 +11,14 @@ class User:
         return hashlib.sha256(password.encode()).hexdigest()
 
     def register(self, db):
-        try:
-            db.cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (self.username, self.password))
-            db.connection.commit()
-            print(color.green("Zarejestrowano użytkownika"))
-        except sqlite3.IntegrityError:
-            print(color.red("Nazwa użytkownika już istnieje wybierz inną"))
+        db.add_user(self.username, self.password)
 
     @staticmethod
     def login(db, username, password):
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
-        db.cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, hashed_password))
-        user = db.cursor.fetchone()
-        if user:
-            print(color.green("Zalogowano!"))
+        user = db.get_user(username)
+        if user and user[2] == hashlib.sha256(password.encode()).hexdigest():
+            print(color.green("Zalogowano pomyślnie!"))
             return True
         else:
-            print(color.red("Nieprawidłowe dane logownia"))
+            print(color.red("Błędna nazwa użytkownika lub hasło."))
             return False
--- database/db.py
+++ database/db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import colors.colors as color
 
 class Database:
     def __init__(self, db_name="app.db"):
@@ -14,7 +15,46 @@ class Database:
                 password TEXT NOT NULL
             )
         ''')
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS players (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE,
+                password TEXT
+            )
+        ''')
+
         self.connection.commit()
 
     def close(self):
         self.connection.close()
+
+    def add_player(self, name, wins):
+        c = self.connection.cursor()
+        try:
+            c.execute('INSERT INTO players (name, wins) VALUES (?, ?)', (name, wins))
+        except sqlite3.IntegrityError:
+            c.execute('UPDATE players SET wins = wins + ? WHERE name = ?', (wins, name))
+        self.connection.commit()
+
+    def show_top(self):
+        c = self.connection.cursor()
+        self.connection.commit('SELECT name, wins FROM players ORDER BY wins DESC LIMIT 100')
+        rows = c.fetchall()
+        print(color.yellow("\nTOP 100 Graczy:"))
+        for row in rows:
+            print(f"{row[0]}: {row[1]} PLN")
+        print()
+
+    def add_user(self, username, password):
+        c = self.connection.cursor()
+        try:
+            c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+            self.connection.commit()
+        except sqlite3.IntegrityError:
+            print(color.red("Użytkownik już istnieje!"))
+
+    def get_user(self, username):
+        c = self.connection.cursor()
+        c.execute("SELECT * FROM users WHERE username = ?", (username,))
+        return c.fetchone()
+
